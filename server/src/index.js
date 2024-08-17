24d1a122d87e92692025f1ca856914f07c883655
IYStoychev21 <IYStoychev21@codingburgas.bg>
2024-08-17 15:27:35 +0300

    Move port to env variable

--- server/src/index.js
+++ server/src/index.js
@@ -65,6 +65,6 @@ app.get('/', (req, res) => {
   res.send('Hello World');
 });
 
-app.listen(3000, () => {
+app.listen(process.env.PORT, () => {
   console.log('Server is running on http://localhost:3000');
 });
