41693786e1b91aef1e3086ef04c7a0873434c8c4
IYStoychev21 <IYStoychev21@codingburgas.bg>
2024-08-17 14:53:55 +0300

    Remove ts

--- server/src/index.js
+++ server/src/index.js
@@ -1,9 +1,7 @@
 import express from 'express';
-import type { Request, Response, NextFunction } from 'express';
 import session from 'express-session';
 import passport from 'passport';
 import { Strategy as GitHubStrategy } from 'passport-github2';
-import type { Profile } from 'passport';
 import cors from 'cors';
 
 const app = express();
@@ -26,7 +24,7 @@ passport.serializeUser((user, done) => {
   done(null, user);
 });
 
-passport.deserializeUser((obj: Express.User, done) => {
+passport.deserializeUser((obj, done) => {
   done(null, obj);
 });
 
@@ -35,7 +33,7 @@ passport.use(new GitHubStrategy({
   clientSecret: process.env.GITHUB_CLIENT_SECRET || 'GITHUB_CLIENT_SECRET',
   callbackURL: process.env.GITHUB_CALLBACK_URL || 'http://localhost:3000/auth/github/callback'
 },
-(accessToken: string, refreshToken: string, profile: Profile, done: any) => {
+(accessToken, refreshToken, profile, done) => {
   const user = { profile, accessToken };
   return done(null, user);
 }));
@@ -45,7 +43,7 @@ app.get('/auth/github',
 
 app.get('/auth/github/callback',
   passport.authenticate('github', { failureRedirect: '/' }),
-  (req: Request, res: Response) => {
+  (req, res) => {
     if (req.user) {
       // @ts-ignore
       res.redirect(`github-widgets://auth?token=${req.user.accessToken}`);
@@ -54,7 +52,7 @@ app.get('/auth/github/callback',
     }
   });
 
-app.get('/logout', (req: Request, res: Response) => {
+app.get('/logout', (req, res) => {
   req.logout((err) => {
     if (err) {
       return res.status(500).send('Error logging out');
@@ -63,7 +61,7 @@ app.get('/logout', (req: Request, res: Response) => {
   });
 });
 
-app.get('/', (req: Request, res: Response) => {
+app.get('/', (req, res) => {
   res.send('Hello World');
 });
 
